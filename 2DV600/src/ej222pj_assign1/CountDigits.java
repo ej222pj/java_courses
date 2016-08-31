d15774366819185fc2085597cc2bcd96155119a6
ej222pj <ej222pj@student.lnu.se>
2016-08-31 15:23:37 +0200

    Done with 3, started 4

--- 2DV600/src/ej222pj_assign1/CountDigits.java
+++ 2DV600/src/ej222pj_assign1/CountDigits.java
@@ -18,31 +18,64 @@ import java.util.Scanner;
 public class CountDigits {
 	public static void main(String[] args) {
 		Scanner ScannerInput;
-		int zeros = 0;
-		int odd = 0;
-		int even = 0;
-		int sum = 0;
+		String exit = "exit";
+		System.out.println("Exit the program by typing: " + exit);
 		
 		while(true){
 			System.out.print("Provide a positive integer: ");
 			//Open a scanner to scan for an input
 			ScannerInput = new Scanner(System.in);
+			int zeros = 0;
+			int odd = 0;
+			int even = 0;
+			int sum = 0;
 			
 			//Save input as string
-			String StringN = ScannerInput.nextLine();
-			int DigitCounter = 0;
-			
-			//Try to parse the input
-			try{
-				//If the parse fail or the number is negative, quit the program.
-				DigitCounter = Integer.parseInt(StringN);
-			    if(DigitCounter < 0)
-					break;
-			} catch (NumberFormatException e) {
-			    break;
+			String StringCount = ScannerInput.nextLine();
+			//Stop the program if the user type exit
+			if(StringCount.equals(exit)){
+				break;
 			}
 			
-			//Close the scanner
-		}	ScannerInput.close();
+			//Split the array on every number
+			String[] SplittedArray = StringCount.split("");
+			
+			//Loop the array to find zero, odd, even and sum
+			for (String didgit  : SplittedArray) {
+				int parsedInt = 0;
+				//Try to parse the input
+				try{
+					//If the parse fail or the number is negative, quit the program.
+					parsedInt = Integer.parseInt(didgit);
+				    if(parsedInt < 0)
+						break;
+				} catch (NumberFormatException e) {
+				    break;
+				}
+				//Add Sum
+				sum = sum + parsedInt;
+				
+				//If the sum is 0, the integer does not start on a number greater then 0.
+				if(sum != 0){
+					//If the number is 0
+					if(parsedInt == 0){
+						zeros++;
+					}
+					//If the number is even
+					else if((parsedInt % 2) == 0){
+						even++;
+					}
+					//If the number is odd
+					else{
+						odd++;
+					}
+				}
+			}
+			System.out.println("Zeros: " + zeros + "\nOdd: " + odd + "\nEven: " + even + "\nSum: " + sum);			
+		}
+		//Close the scanner
+		ScannerInput.close();
+		System.out.print("Exiting!");
+		
 	}
 }
